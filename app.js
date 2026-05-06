93b203bb49d426152108785df081cf4ee993ac27
Payal Bisen <payal@gmail.com>
2026-05-06 13:13:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+// // push() last element added of array
+
+let number = [1,2,3,4,5];
+ let addEnd = number.push(6,7,8);
+  console.log(number);
+  console.log(addEnd);
+
+
+
+//   unshift first elements add of array
+
+  let courses= ["html", "css", "js", ];
+ let addFirst = courses.unshift("java", "c", "c++", "python");
+  console.log(courses);
+  console.log(addFirst);
+  
+
+//   pop() last element remove of array
+
+let fruits = ["apple", "mango", "banana", "orange"];
+ let subEnd = fruits.pop();
+  console.log(fruits);
+  console.log(subEnd);
+
+
+
+//   shift first element remove of array
+
+  let course= ["html", "css", "js"];
+ let subFirst = courses.shift();
+  console.log(courses);
+  console.log(addFirst);
+
+
+
+
